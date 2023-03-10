0b423fb9dd771f28ed89b9561541c62bbdc6938d
MohamedFawzy <mfawzy22@gmail.com>
2023-03-11 02:52:55 +0400

    add neotree

--- init.lua
+++ init.lua
@@ -492,6 +492,7 @@ cmp.setup {
     { name = 'luasnip' },
   },
 }
+vim.keymap.set("n", "<leader>t", ':Neotree toggle<CR>')
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
--- lua/custom/plugins/filetree.lua
+++ lua/custom/plugins/filetree.lua
@@ -9,7 +9,20 @@ return {
     "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
     "MunifTanjim/nui.nvim",
   },
-  config = function ()
-    require('neo-tree').setup {}
+  config = function()
+    require('neo-tree').setup({
+      close_if_last_window = false,
+      enable_diagnostics = true,
+      enable_git_status = true,
+      popup_border_style = "rounded",
+      sort_case_insensitive = false,
+      filesystem = {
+        filtered_items = {
+          hide_dotfiles = false,
+          hide_gitignored = false,
+        },
+      },
+      window = { width = 30 },
+    })
   end,
 }
