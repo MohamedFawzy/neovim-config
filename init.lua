856a1f41a3b228f51a1f8edd38aa398a155f6c0a
MohamedFawzy <mfawzy22@gmail.com>
2023-03-11 03:21:18 +0400

    change keymap for neotree and add toggle term plugin

--- init.lua
+++ init.lua
@@ -492,7 +492,7 @@ cmp.setup {
     { name = 'luasnip' },
   },
 }
-vim.keymap.set("n", "<leader>t", ':Neotree toggle<CR>')
+vim.keymap.set("n", "<leader>f", ':Neotree toggle<CR>')
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
