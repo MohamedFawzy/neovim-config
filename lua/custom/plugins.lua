cb131ad81a3b821942b9211ef8651ae1b43017ed
MohamedFawzy <mfawzy22@gmail.com>
2024-04-09 03:44:03 +0400

    add lspconfigs for nodejs to support autocomplete

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -7,6 +7,13 @@ local plugins = {
       }
     }
   },
+  {
+    "neovim/nvim-lspconfig",
+    config = function ()
+     require "plugins.configs.lspconfig"
+     require "custom.configs.lspconfig"
+    end,
+  },
 }
 
 return plugins
