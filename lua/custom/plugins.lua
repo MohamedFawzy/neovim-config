af59801be93b8eaf713c840eb40f09880f68c208
MohamedFawzy <mfawzy22@gmail.com>
2024-05-19 17:48:33 +0400

    enable gopher

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -89,6 +89,16 @@ local plugins = {
       require "custom.configs.lspconfig"
     end,
   },
+  {
+    "olexsmir/gopher.nvim",
+    ft = "go",
+    config = function(_, opts)
+      require("gopher").setup(opts)
+    end,
+    build = function()
+      vim.cmd [[silent! GoInstallDeps]]
+    end,
+  }
 }
 
 return plugins
