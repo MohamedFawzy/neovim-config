e49f8c34d3f8ad255ec656013a10db2ec5a92d84
MohamedFawzy <mfawzy22@gmail.com>
2024-05-19 15:38:52 +0400

    adding lsp configs for golang

--- lua/custom/configs/lspconfig.lua
+++ lua/custom/configs/lspconfig.lua
@@ -3,8 +3,9 @@ local on_attach = config.on_attach
 local capabilities = config.capabilities
 
 local lspconfig = require("lspconfig")
+local util = require "lspconfig/util"
 
-lspconfig.tsserver.setup ({
+lspconfig.tsserver.setup({
   on_attach = on_attach,
   capabilities = capabilities,
   init_options = {
@@ -15,8 +16,24 @@ lspconfig.tsserver.setup ({
 })
 
 
-lspconfig.pyright.setup ({
+lspconfig.pyright.setup({
   on_attach = on_attach,
   capabilities = capabilities,
-  filetypes = {"python"},
+  filetypes = { "python" },
+})
+
+
+lspconfig.gopls.setup({
+  on_attach = on_attach,
+  capabilities = capabilities,
+  cmd = { "gopls" },
+  filetypes = { "go", "gomod", "gowork", "gotmpl" },
+  root_dir = util.root_pattern("go.work", "go.mod", ".git"),
+  settings = {
+    completedUnimported = true,
+    usePlaceholders = true,
+    analysis = {
+      unusedparams = true,
+    }
+  }
 })
